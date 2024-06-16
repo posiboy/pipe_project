a38887c4beb8a8445afa3983f40c04c2ddaf9285
Deju Adejo <ojomideju2003@gmail.com>
2024-06-16 22:20:48 +0100

    Create registration functionality

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
+from werkzeug.security import generate_password_hash, check_password_hash
 from forms import LoginForm, UploadForm, RegisterForm
 
 app = Flask(__name__)
@@ -61,7 +62,36 @@ def upload():
 def register():
     register_form = RegisterForm()
     if register_form.validate_on_submit():
-        pass
+        # Verifying new user by email
+        user = db.session.execute(db.Select(User).where(User.email == register_form.email.data)).scalar()
+        if user:
+            flash('Account already exists!')
+            return redirect(url_for('login'))
+
+        # Verify user password if user is new
+        elif register_form.password.data != register_form.confirm_password.data:
+            flash('Password does not match! Confirm password before submitting.')
+            return redirect(url_for('register'))
+
+        # Proceed if password matches
+        # Hash the user's password for security
+        hashed_password = generate_password_hash(
+            register_form.password.data,
+            method="pbkdf2:sha256",
+            salt_length=8,  # Salting for password
+        )
+
+        # Create User object for new user
+        new_user = User(
+            name=register_form.name.data,
+            email=register_form.email.data,
+            password=hashed_password
+        )
+
+        # Add the user to the database
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('home'))
     return render_template('register.html', form=register_form)
 
 
